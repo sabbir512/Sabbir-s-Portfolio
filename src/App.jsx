c483cc68cb2f12be10d8809babf134c83b41395b
Sabbir <sabbirkhan.kst178@gamil.com>
2024-05-11 08:20:59 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <h1 className="bg-red-500">HELLO WORLD</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae officiis
+        nobis sit.
+      </p>
+    </>
+  );
+}
+
+export default App;
