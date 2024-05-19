982afc1fb301da4eb799789c32036f7fcc843644
Sabbir <sabbirkhan.kst178@gamil.com>
2024-05-19 22:19:48 +0600

    change grid column

--- src/components/ExpContent.jsx
+++ src/components/ExpContent.jsx
@@ -17,21 +17,15 @@ import {
 function ExpContent() {
   return (
     <div className="text-white w-[100%] text-center pt-5 grid gap-y-8">
-      <div className="grid sm:grid-cols-4 justify-items-center content-center smallerPhone:grid-cols-2 smallerPhone:gap-y-6">
+      <div className="grid sm:grid-cols-4 justify-items-center content-center smallerPhone:grid-cols-2 smallerPhone:gap-y-6 mediumPhone:grid-cols-3">
         <ExpImage Logos={htmlLogo} logoName="HTML5" />
         <ExpImage Logos={cssLogo} logoName="CSS3" />
         <ExpImage Logos={tailwindLogo} logoName="TailwindCSS" />
         <ExpImage Logos={javaScriptLogo} logoName="JavaScript" />
-      </div>
-
-      <div className="grid sm:grid-cols-4 justify-items-center content-center smallerPhone:grid-cols-2 smallerPhone:gap-y-6">
         <ExpImage Logos={reactLogo} logoName="ReactJs" />
         <ExpImage Logos={typeScriptLogo} logoName="TypeScript" />
         <ExpImage Logos={nodeLogo} logoName="NodeJS" />
         <ExpImage Logos={expressJsLogo} logoName="ExpressJS" />
-      </div>
-
-      <div className="grid sm:grid-cols-4 justify-items-center content-center smallerPhone:grid-cols-2 smallerPhone:gap-y-6">
         <ExpImage Logos={figmaLogo} logoName="Figma" />
         <ExpImage Logos={mongoLogo} logoName="MongoDB" />
         <ExpImage Logos={gitLogo} logoName="Git" />
