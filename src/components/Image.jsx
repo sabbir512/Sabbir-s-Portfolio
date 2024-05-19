199556b90c74d2362e08d6de38a94019d8d81ba5
Sabbir <sabbirkhan.kst178@gamil.com>
2024-05-19 22:35:03 +0600

    Make changes at image component for medium phone responsive

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -8,7 +8,7 @@ function Image() {
         <img
           src={myImage}
           alt="Image"
-          className="lg:w-[439px] lg:h-[460px] lagerThenLg:w-[459px] lagerThenLg:h-[480px] md:w-[402px] md:h-[433px] sm:w-[422px] sm:h-[443px] mediumPhone:w-[252px] mediumPhone:h-[273px] bg-no-repeat bg-center  transform -translate-x-4 -translate-y-4 right-0 bottom-0 rounded-md  smallerPhone:w-[180px] smallerPhone:h-[185px]"
+          className="lg:w-[439px] lg:h-[460px] lagerThenLg:w-[459px] lagerThenLg:h-[480px] md:w-[402px] md:h-[433px] sm:w-[422px] sm:h-[443px] mediumPhone:w-[252px] mediumPhone:h-[255px] bg-no-repeat bg-center  transform -translate-x-4 -translate-y-4 right-0 bottom-0 rounded-md  smallerPhone:w-[180px] smallerPhone:h-[185px]"
         />
       </div>
     </div>
