8885f3fa3b879636ede1c177b9c8d92f00a33e31
Sabbir <sabbirkhan.kst178@gamil.com>
2024-06-13 22:24:00 +0600

    Trying to set overflow for mobile phone

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Navbar, Footer} from "./components/index";
 import { Outlet } from "react-router-dom";
 function App() {
   return (
-    <div>
+    <div className="overflow-hidden">
       <Navbar />
       <Outlet />
       <Footer />
